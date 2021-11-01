40613c9817804a04447c8395471f9692ea3fb217
Chris Purta <christopher.purta@formation.ai>
2021-10-31 22:42:12 -0700

    Remove singleton pattern from StateKeeper too

--- packages/hydra-processor/src/state/index.ts
+++ packages/hydra-processor/src/state/index.ts
@@ -4,12 +4,9 @@ import { StateKeeper } from './StateKeeper'
 export * from './IStateKeeper'
 export * from './StateKeeper'
 
-let stateKeeper: StateKeeper
-
 export async function getStateKeeper(indexerEndpointURL: string): Promise<IStateKeeper> {
-  if (!stateKeeper) {
-    stateKeeper = new StateKeeper()
-    await stateKeeper.init(indexerEndpointURL)
-  }
+  const stateKeeper = new StateKeeper()
+  await stateKeeper.init(indexerEndpointURL)
+  
   return stateKeeper
 }
